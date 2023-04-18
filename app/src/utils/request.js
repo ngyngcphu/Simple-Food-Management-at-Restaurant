7dfa7875c55852396922dad2ccbb169c7888297d
ngyngcphu <nguyenngocphu225@gmail.com>
2023-04-18 21:48:34 +0700

    done db.json

--- app/src/utils/request.js
+++ app/src/utils/request.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const request = axios.create({
-  //baseURL: "https://my-json-server.typicode.com/ngyngcphu/Simple-Food-Management-at-Restaurant"
-  baseURL: "http://localhost:3001"
+  baseURL: "https://my-json-server.typicode.com/ngyngcphu/Simple-Food-Management-at-Restaurant"
+  //baseURL: "http://localhost:3001"
 });
