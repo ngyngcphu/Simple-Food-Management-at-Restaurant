949ce2f0bc137bbc7b8247623bcb2db5044a2ecd
ngyngcphu <nguyenngocphu225@gmail.com>
2023-04-18 21:25:57 +0700

    wtf deploy ???

--- app/src/utils/request.js
+++ app/src/utils/request.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const request = axios.create({
   //baseURL: "https://my-json-server.typicode.com/ngyngcphu/Simple-Food-Management-at-Restaurant"
-  baseURL: "http://localhost:3001"
+  baseURL: "http://localhost:3001",
 });
