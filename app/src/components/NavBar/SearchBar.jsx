6c935fe79649b8e0be76fa3374acec6db83779c0
quannhg <128902312+quannhg@users.noreply.github.com>
2023-04-24 20:29:08 +0700

    feat: search bar show food name

---
+++ app/src/components/NavBar/SearchBar.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import "./SearchBar.css";
+import { AiOutlineSearch as SearchIcon } from "react-icons/ai";
+import { GrClose as CloseIcon } from "react-icons/gr";
+
+function SearchBar({ placeholder, data }) {
+    const [filteredData, setFilteredData] = useState([]);
+    const [wordEntered, setWordEntered] = useState("");
+
+    const handleFilter = (event) => {
+        const searchWord = event.target.value;
+        setWordEntered(searchWord);
+        const newFilter = data.filter((value) => {
+            return value.name.toLowerCase().includes(searchWord.toLowerCase());
+        });
+
+        if (searchWord === "") {
+            setFilteredData([]);
+        } else {
+            setFilteredData(newFilter);
+        }
+    };
+
+    const clearInput = () => {
+        setFilteredData([]);
+        setWordEntered("");
+    };
+
+    return (
+        <div className="search">
+            <div className="searchInputs">
+                <input
+                    type="text"
+                    placeholder={placeholder}
+                    value={wordEntered}
+                    onChange={handleFilter}
+                />
+                <div className="searchIcon">
+                    {filteredData.length === 0 ? (
+                        <SearchIcon />
+                    ) : (
+                        <CloseIcon id="clearBtn" onClick={clearInput} />
+                    )}
+                </div>
+            </div>
+            {filteredData.length !== 0 && (
+                <div className="dataResult">
+                    {filteredData.slice(0, 15).map((value, key) => {
+                        return (
+                            <a className="dataItem" href={value.link} target="_blank" rel="noreferrer">
+                                <p>{value.name} </p>
+                            </a>
+                        );
+                    })}
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default SearchBar;
--- app/src/utils/request.js
+++ app/src/utils/request.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const request = axios.create({
-  baseURL: "https://tickflow.net",
+  baseURL: "https://ltnc-server.tickflow.net",
   //baseURL: "http://localhost:3001"
 });
