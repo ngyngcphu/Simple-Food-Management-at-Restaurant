404d19bf8b51baf30e4fb3130ab7f05926c14edd
quannhg <128902312+quannhg@users.noreply.github.com>
2023-04-29 09:27:34 +0700

    feat: click on dataitem (searchitem) to open food detail

--- app/src/components/NavBar/SearchBar.jsx
+++ app/src/components/NavBar/SearchBar.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./SearchBar.css";
+import { FoodDetailOnSearch } from "../FoodDetail"
 import { AiOutlineSearch as SearchIcon } from "react-icons/ai";
 import { GrClose as CloseIcon } from "react-icons/gr";
 
@@ -9,6 +10,8 @@ export class SearchBar extends Component {
         this.state = {
             filteredData: [],
             wordEntered: "",
+            showFoodDetail: false,
+            selectedFoodId: null,
         };
     }
 
@@ -44,44 +47,82 @@ export class SearchBar extends Component {
         });
     };
 
+    // set show state to true and selectedFoodId when dataItem is clicked
+    handleDataItemClick = (foodId) => {
+        this.setState({ showFoodDetail: true, selectedFoodId: foodId });
+    };
+
+    // set show state to false when close button in FoodDetailOnSearch is clicked
+    handleCloseFoodDetail = () => {
+        this.setState({ showFoodDetail: false });
+    };
+
     render() {
         const { placeholder } = this.props;
         const { filteredData, wordEntered } = this.state;
 
         return (
-            <div className="search">
-                <div className="searchInputs">
-                    <input
-                        type="text"
-                        placeholder={placeholder}
-                        value={wordEntered}
-                        onChange={this.handleFilter}
-                    />
-                    <div className="searchIcon">
-                        {filteredData.length === 0 ? (
-                            <SearchIcon />
-                        ) : (
-                            <CloseIcon id="clearBtn" onClick={this.clearInput} />
-                        )}
+            <div>
+                <div className="search">
+                    <div className="searchInputs">
+                        <input
+                            type="text"
+                            placeholder={placeholder}
+                            value={wordEntered}
+                            onChange={this.handleFilter}
+                        />
+                        <div className="searchIcon">
+                            {filteredData.length === 0 ? (
+                                <SearchIcon />
+                            ) : (
+                                <CloseIcon id="clearBtn" onClick={this.clearInput} />
+                            )}
+                        </div>
                     </div>
+                    {filteredData.length !== 0 && (
+                        <div className="dataResult">
+                            {filteredData.slice(0, 15).map((value, key) => {
+                                return (
+                                    <div
+                                        key={key}
+                                        className="dataItem"
+                                        target="_blank"
+                                        rel="noreferrer"
+                                        onClick={() => this.handleDataItemClick(value.id)} // handleDataItemClick is called when dataItem is clicked
+                                    >
+                                        <p>{value.name}</p>
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    )}
                 </div>
-                {filteredData.length !== 0 && (
-                    <div className="dataResult">
-                        {filteredData.slice(0, 15).map((value, key) => {
-                            return (
-                                <a
-                                    key={key}
-                                    className="dataItem"
-                                    href={value.link}
-                                    target="_blank"
-                                    rel="noreferrer"
-                                >
-                                    <p>{value.name}</p>
-                                </a>
-                            );
-                        })}
-                    </div>
+
+                {this.state.showFoodDetail && (
+                    // show FoodDetailOnSearch when showFoodDetail state is true
+                    <FoodDetailOnSearch
+                        id={this.state.selectedFoodId}
+                        name={
+                            filteredData.find((f) => f.id === this.state.selectedFoodId)?.name // add a check for the 'name' property
+                        }
+                        price={
+                            filteredData.find((f) => f.id === this.state.selectedFoodId)?.price // add a check for the 'price' property
+                        }
+                        discount={
+                            filteredData.find((f) => f.id === this.state.selectedFoodId)?.discount // add a check for the 'discount' property
+                        }
+                        imageUrls={
+                            filteredData.find((f) => f.id === this.state.selectedFoodId)?.imageUrls // add a check for the 'imageUrls' property
+                        }
+                        description={
+                            filteredData.find((f) => f.id === this.state.selectedFoodId)?.description // add a check for the 'description' property
+                        }
+                        foodData={this.props.data}
+                        showFoodDetail={this.state.showFoodDetail}
+                        handleClose={this.handleCloseFoodDetail}
+                    />
                 )}
+
             </div>
         );
     }
